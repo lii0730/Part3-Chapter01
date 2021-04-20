55a076a2c6ebaa35cb2e3c250b7eeee4f545e47d
현석 이 <gustjr9630@naver.com>
2021-04-20 18:23:07 +0900

    neo 0420 commit

--- app/src/main/java/com/example/aop_part3_chapter01/MainActivity.kt
+++ app/src/main/java/com/example/aop_part3_chapter01/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.aop_part3_chapter01
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val Tag = "FirebaseService"
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val tokenTextView: TextView by lazy {
+        findViewById(R.id.tokenTextView)
+    }
+
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        getToken()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
+    }
+
+    private fun getToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                tokenTextView.text = task.result
+            }
+        })
+    }
+
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
     }
 }
